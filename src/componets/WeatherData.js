b34a6995f87149ced1f9874695ff0aafccd45cc2
Abdul Basit <abdul.basit@medznmore.com>
2021-07-23 15:17:50 +0500

    issue in getting object property

---
+++ src/componets/WeatherData.js
@@ -0,0 +1,34 @@
+import React,{useContext} from 'react'
+import Context from '../Context';
+
+function WeatherData() {
+    const weather =useContext(Context);
+    const{temp='',humidity='',pressure=''}= weather;
+    console.log(temp);
+    console.log(weather);
+
+    return (
+        <div>
+            <div className="weather-data">
+                <p className="weather__tagline">Weather forecast for <span className="weather-data__city">Manchester</span></p>
+                <div className="weather-data__box">
+                    <span className="weather-data__property">
+                        <p className="weather-data__title">Temperature</p>
+                        <p className="weather-data__value">{temp}</p>
+                    </span>
+                    <span className="weather-data__property">
+                        <p className="weather-data__title">Humidity</p>
+                        <p className="weather-data__value">{humidity}</p>
+                    </span>
+                    <span className="weather-data__property">
+                        <p className="weather-data__title">Pressure</p>
+                        <p className="weather-data__value">{pressure}</p>
+                    </span>
+                </div>
+            </div>
+
+        </div>
+    )
+}
+
+export default WeatherData
